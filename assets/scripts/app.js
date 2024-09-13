e7ed49eb95ddda1367be031eae36b887f37fe8d5
Thiago <thiagodeboliveira@outlook.com>
2024-09-13 18:24:43 -0300

    Estudos react,recapitulando javascript

---
+++ assets/scripts/app.js
@@ -0,0 +1,59 @@
+// import  apiKey  from "./util.js";
+
+// console.log(apiKey);
+
+// let userMesage = ("hellow World");
+
+// console.log(userMesage);
+// if (10 === 10){
+//     console.log("Verdade TA OK")
+// }
+
+// function createGreeting(userName, message = "Seja bem vindo" ) {
+//     // console.log(userName);
+//     // console.log(message);
+//     return "Ola, eu sou" + userName + "-" + message;
+// }
+// const greeting1 = createGreeting("Thiago")
+// console.log(greeting1);
+
+// export default ()=>{
+//     console.log("HELLOW")
+// }
+
+// const user = {
+//     name: "Thiago",
+//     age: 18,
+//     greet() {
+//         console.log("Hello")
+//         console.log(this.age)
+//     }
+// };
+
+// console.log(user.name);
+// user.greet();
+
+// class User {
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     greet(){
+//         console.log("Hi")
+//     }
+// }
+
+// const user1 = new User("Manuel", 38);
+// console.log(user1);
+
+// const hobbies = ["sports","games","programming","travels"];
+// console.log(hobbies[3]);
+
+// hobbies.push("working")
+// console.log(hobbies);
+
+// const index = hobbies.findIndex((item) => item === "games")
+// console.log(index);
+
+// const maping = hobbies.map((item) => item + "!");
+// console.log(maping)
